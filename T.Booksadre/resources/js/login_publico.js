c57297fb27ed976aff88c9f7d6dec5af56af023b
Adiel Montepeque <137818505+AdielMontepeque1@users.noreply.github.com>
2024-03-20 20:39:31 -0600

    .

---
+++ T.Booksadre/resources/js/login_publico.js
@@ -0,0 +1,7 @@
+function redirectToRegistro() {
+    window.location.href = "../../views/public/registro.html";
+}
+
+function regresar() {
+    window.location.href = "../../views/public/index.html";
+}
