23ff19f6ab32681cc698952f45f2802ce54b3b88
Adiel Montepeque <137818505+AdielMontepeque1@users.noreply.github.com>
2024-03-21 08:14:01 -0600

    .

---
+++ T.Booksadre/resources/js/registro.js
@@ -0,0 +1,7 @@
+function redirectToRegistro() {
+    window.location.href = "../../views/public/index.html";
+}
+
+function regresar() {
+    window.location.href = "../../views/public/login.html";
+}
