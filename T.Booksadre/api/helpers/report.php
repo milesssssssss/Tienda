6e6504a734fec4cff7f696721382bb7dbc4e8859
Josue <20220619@ricaldone.edu.sv>
2024-05-02 08:21:20 -0600

    maycol

--- T.Booksadre/api/helpers/report.php
+++ T.Booksadre/api/helpers/report.php
@@ -9,7 +9,7 @@ require_once('../../libraries/fpdf185/fpdf.php');
 class Report extends FPDF
 {
     // Constante para definir la ruta de las vistas del sitio privado.
-    const CLIENT_URL = 'http://localhost/coffeeshop/views/admin/';
+    const CLIENT_URL = 'http://localhost/Tienda/T.Booksadre/views/admin/index.html';
     // Propiedad para guardar el título del reporte.
     private $title = null;
 
